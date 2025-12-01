ba19176264ffcb85dfb033d54ab38c5d29ba9fea
Arafath-R <arfath.r.cse079@gmail.com>
2025-12-01 11:57:25 +0600

    Done Practise

--- scripts/add_money.js
+++ scripts/add_money.js
@@ -6,11 +6,40 @@ document.getElementById('add-money').addEventListener('click', function (event) 
     const addAmount = getInputValueById('amount')
     const pin = getInputValueById('pin-num')
     const mainBalance = getInnerTextById('balance')
+    const selectBank = document.getElementById('allBank').value
+
+    if(addAmount < 0){
+        alert('Please enter positive number')
+        return;
+    }
+
 
     if (accNumber.length === 11) {
         if (pin === 1235) {
             const newBalance = addAmount + mainBalance
             setInnerTextByIdandValue('balance',newBalance)
+
+
+            const container = document.getElementById('transaction')
+
+            const div = document.createElement('div')
+            div.innerHTML = `
+            <div class="bg-white flex justify-between p-5 items-center rounded-xl border">
+                    <div class="flex">
+                        <div class="w-10 rounded-full bg-gray-100 flex justify-center items-center">
+                            <img src="./assets/wallet1.png" />
+                        </div>
+                        <div class="pl-3">
+                            <h2 class="font-bold">Added ${addAmount} From ${selectBank}</h2>
+                            <p class="text-xs">${new Date()}</p>
+                        </div>
+                    </div>
+                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                </div> 
+            `
+            container.appendChild(div)
+
+
         }
         else {
             alert('enter pin correctly')
--- scripts/cash-out.js
+++ scripts/cash-out.js
@@ -5,10 +5,39 @@ document.getElementById('cashout-btn').addEventListener('click',function(event){
     const pin = getInputValueById('pin-num')
     const mainBalance = getInnerTextById('balance')
 
+    if(amountWithdraw > mainBalance){
+        alert('insufficient balance')
+        return;
+    }
+
+
     if(agentNumber.length === 11){
         if(pin === 1235){
             const newBalance = mainBalance - amountWithdraw
             setInnerTextByIdandValue('balance',newBalance)
+
+
+            const container = document.getElementById('transaction')
+
+            const div = document.createElement('div')
+            div.innerHTML = `
+            <div class="bg-white flex justify-between p-5 items-center rounded-xl border">
+                    <div class="flex">
+                        <div class="w-16 rounded-full bg-gray-100 flex justify-center items-center">
+                            <img src="./assets/send1.png" />
+                        </div>
+                        <div class="pl-3">
+                            <h2 class="font-bold">Cash Out ${amountWithdraw} From ${agentNumber}</h2>
+                            <p class="text-xs">${new Date()}</p>
+                        </div>
+                    </div>
+                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                </div> 
+            `
+            container.appendChild(div)
+
+
+
         }
         else{
             alert('enter pin correctly');  
